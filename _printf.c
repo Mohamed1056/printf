2e22358a484a373a8b276de03afd574e87756a3a
Boodenator <boode1000@gmail.com>
2023-08-22 05:02:25 -0700

    task0

--- _printf.c
+++ _printf.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <stdio.h>
 
 void print_buffer(char buffer[], int *buff_ind);
 
@@ -64,4 +63,4 @@ void print_buffer(char buffer[], int *buff_ind)
 		write(1, &buffer[0], *buff_ind);
 
 	*buff_ind = 0;
-} 
+}
